1f30e7eef0d17ac4babc713d1cb07ccd0855fd23
FakirHerif <basdemir35@hotmail.com.tr>
2023-12-08 21:23:50 +0300

    added jss content

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sagListe = document.getElementById("SagListe");
+    const loading = document.getElementById("loading");
+    const solForm = document.getElementById("solForm");
+
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+        .then(response => response.json())
+        .then(data => {
+            loading.style.display = "none";
+
+            data.forEach(todo => {
+                const listItem = document.createElement("li");
+                listItem.textContent = `${todo.title} - ${todo.completed ? 'Completed' : 'Not Completed'}`;
+
+                const deleteButton = document.createElement("button");
+                deleteButton.textContent = "Delete";
+                deleteButton.addEventListener("click", function() {
+                    listItem.remove();
+                });
+
+                listItem.appendChild(deleteButton);
+                sagListe.appendChild(listItem);
+            });
+        })
+        .catch(error => {
+            console.log("Veri alınamadı! Hata: " + error.message);
+        });
+
+
+    solForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const text = document.getElementById("text").value;
+        const checkbox = document.getElementById("checkbox").checked;
+
+        const listItem = document.createElement("li");
+        listItem.textContent = `${text} - ${checkbox ? 'Completed' : 'Not Completed'}`;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function() {
+            listItem.remove();
+        });
+
+        listItem.appendChild(deleteButton);
+        sagListe.appendChild(listItem);
+
+        solForm.reset();
+    });
+});
